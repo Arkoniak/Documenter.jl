f5e309eaa06f8cec8e146f4d1fc8904d9773b560
Andrey Oskin <skoffer@arkoniak.com>
2020-06-09 22:15:25 +0300

    added link to main repo site

--- src/Writers/HTMLWriter.jl
+++ src/Writers/HTMLWriter.jl
@@ -381,6 +381,7 @@ struct HTML <: Documenter.Writer
     disable_git   :: Bool
     edit_link     :: Union{String, Symbol, Nothing}
     canonical     :: Union{String, Nothing}
+    siteurl       :: String
     assets        :: Vector{HTMLAsset}
     analytics     :: String
     collapselevel :: Int
@@ -396,6 +397,7 @@ struct HTML <: Documenter.Writer
             disable_git   :: Bool = false,
             edit_link     :: Union{String, Symbol, Nothing, Default} = Default("master"),
             canonical     :: Union{String, Nothing} = nothing,
+            siteurl       :: String = "",
             assets        :: Vector = String[],
             analytics     :: String = "",
             collapselevel :: Integer = 2,
@@ -433,7 +435,7 @@ struct HTML <: Documenter.Writer
             end
         end
         isa(edit_link, Default) && (edit_link = edit_link[])
-        new(prettyurls, disable_git, edit_link, canonical, assets, analytics,
+        new(prettyurls, disable_git, edit_link, canonical, siteurl, assets, analytics,
             collapselevel, sidebar_sitename, highlights, mathengine, footer, lang, warn_outdated)
     end
 end
@@ -967,7 +969,9 @@ function render_sidebar(ctx, navnode)
     # Sitename
     if ctx.settings.sidebar_sitename
         push!(navmenu.nodes, div[".docs-package-name"](
-            span[".docs-autofit"](a[:href => href](ctx.doc.user.sitename))
+           span[".docs-autofit"](isempty(ctx.settings.siteurl) ?
+                                    a[:href => href](ctx.doc.user.sitename) :
+                                    a[:href => ctx.settings.siteurl](ctx.doc.user.sitename))
         ))
     end
 
